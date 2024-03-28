2d0aee4bc93e1a8e9182525dc021f02637fdc312
Arthur <arthurferreira1417@gmail.com>
2024-03-28 17:26:56 -0300

    Feat: inicial navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import Pessoas from "./src/pages/pessoas/pessoas";
+import Home from "./src/pages/pessoas/pessoas";
 import { View } from "react-native";
+// rotas
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <View>
       <StatusBar style="light" backgroundColor="transparent" translucent />
-      <Pessoas />
+      <Home/>
+      {/* <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer> */}
     </View>
   );
 }
