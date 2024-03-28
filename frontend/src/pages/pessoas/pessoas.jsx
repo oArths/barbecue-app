ad63fdd491df1c662963164ce8df1ae07bddea0b
Arthur <arthurferreira1417@gmail.com>
2024-03-27 22:03:17 -0300

    Feat: create function selector and start styled to pessoas page

--- frontend/src/pages/pessoas/pessoas.jsx
+++ frontend/src/pages/pessoas/pessoas.jsx
@@ -9,18 +9,50 @@ const Pessoas = () => {
                     <S.Icon source={require("../../img/seta.png")}/>
                 </S.IconButton>
             </S.iconContainer>
-            <S.Title>Selecione o número de pessoas</S.Title>
+            <S.TitleContainer>
+                <S.Title>Selecione as pessoas</S.Title>
+            </S.TitleContainer>
             <S.Conatiner>
                 <S.ConatinerOp>
-                    <S.IconOp source={require("../../img/man.png")}/>
-                    <S.TitleOp>Homem</S.TitleOp>
+                    <S.HaederContainer>
+                        <S.IconOp source={require("../../img/man.png")}/>
+                        <S.TitleOp>Homem</S.TitleOp>
+                    </S.HaederContainer>
                     <S.ConatinerCount>                
                         <Timer/>
                     </S.ConatinerCount>
                 </S.ConatinerOp>
+                <S.ConatinerOp>
+                    <S.HaederContainer>
+                        <S.IconOp source={require("../../img/woman.png")}/>
+                        <S.TitleOp>Mulher</S.TitleOp>
+                    </S.HaederContainer>
+                    <S.ConatinerCount>                
+                        <Timer/>
+                    </S.ConatinerCount>
+                </S.ConatinerOp>
+                <S.ConatinerOp>
+                    <S.HaederContainer>
+                        <S.IconOp source={require("../../img/childer.png")}/>
+                        <S.TitleOp>Criança</S.TitleOp>
+                    </S.HaederContainer>
+                    <S.ConatinerCount>                
+                        <Timer/>
+                    </S.ConatinerCount>
+                </S.ConatinerOp>
+                <S.ConatinerOp>
+                    <S.HaederContainer>
+                        <S.IconAll source={require("../../img/user.png")}/>
+                        <S.TitleOp>Total</S.TitleOp>
+                    </S.HaederContainer>
+                    <S.ConatinerCount>
+                        <S.Total>23</S.Total>       
+                    </S.ConatinerCount>
+                </S.ConatinerOp>
             </S.Conatiner>
             <S.Button>
-            <S.Title>babababa</S.Title>
+            <S.TitleButton>Próximo</S.TitleButton>
+            <S.IconNext source={require("../../img/Union.png")}/>
             </S.Button>
         </S.Body>
     )
--- frontend/src/pages/pessoas/style.jsx
+++ frontend/src/pages/pessoas/style.jsx
@@ -3,19 +3,19 @@ import styled from "styled-components/native"
 export const Body = styled.View`
 display: flex;
 flex-direction: column;
-justify-content: center;
+justify-content: start;
 align-items: center;
 width: 100%;
-background-color: #3700ff;
+height: 100%;
+background-color: #F5F5F5;
+gap: 30px;
 
 `;
 export const iconContainer = styled.View`
 display: flex;
 justify-content: center;
 width: 90%;
-background-color: #ff0088;
-margin-top: 60px;
-
+margin-top: 70px;
 
 `;
 export const IconButton = styled.TouchableOpacity`
@@ -28,45 +28,98 @@ height: 34px;
 
 `;
 export const Icon = styled.Image`
-width: 38px;
-height: 34px;
+width: 30px;
+height: 26px;
 
 
 `;
 export const Button = styled.TouchableOpacity`
+position: absolute;
 display: flex;
-width: 100px;
-color: blueviolet;
-background-color: aqua;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+gap: 10px;
+width: 170px;
+height: 44px;
+border-radius: 10px;
+background-color: #A4161A;
+bottom: 50px;
+
+`;
+export const TitleButton = styled.Text`
+color: #ffffff;
+font-size: 22px;
+
+`;
+export const IconNext = styled.Image`
+width: 15px;
+height: 13px;
+
+`;
+export const TitleContainer = styled.View`
+width: 300px;
 
 `;
 export const Title = styled.Text`
+text-align: center;
 color: black;
-font-size: 22px;
-background-color: aqua;
+font-size: 30px;
 
 `;
+
 export const Conatiner = styled.View`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 90%;
+gap: 30px;
+`;
+export const ConatinerOp = styled.View`
 display: flex;
 flex-direction: row;
 justify-content: center;
-width: 80%;
-background-color: #bf00ff;
+align-items: center;
+background-color: #ffffff;
+border-radius: 15px;
+padding: 20px;
+width: 300px;
+gap: 25px;
+elevation: 2;
 
 `;
-export const ConatinerOp = styled.TouchableOpacity`
-background-color: #9dff00;
-
+export const HaederContainer = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
 `;
 export const TitleOp = styled.Text`
-color: coral;
+font-size: 20px;
+color: #000000;
 
 `;
 export const ConatinerCount = styled.View`
-width: 70px;
-height: 70px;
+display: flex;
+align-items: center;
+justify-content: center;
+width: 110px;
+height: 50px;
+`;
+export const Total = styled.Text`
+text-align: center;
+color: black;
+font-size: 20px;
+margin-left: 30px;
+/* background-color: blue; */
+
 `;
 export const IconOp = styled.Image`
-width: 70px;
-height: 70px;
+width: 30px;
+height: 30px;
+`;
+export const IconAll = styled.Image`
+width: 30px;
+height: 20px;
 `;
