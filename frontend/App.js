b8740f8bdd6c9b486b27c4c8895287846ae75180
Arthur <arthurferreira1417@gmail.com>
2024-03-28 21:57:35 -0300

    Feat: create a routes

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import Home from "./src/pages/pessoas/pessoas";
+import Selecao from "./src/pages/selecao/selecao"
+import Pessoas from "./src/pages/pessoas/pessoas";
 import { View } from "react-native";
-// rotas
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  const Stack = createNativeStackNavigator();
+export default function App () {
+
   return (
     <View>
-      <StatusBar style="light" backgroundColor="transparent" translucent />
-      <Home/>
-      {/* <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer> */}
+      <StatusBar style="light" backgroundColor='transparent' translucent/>
+      <Selecao/>
+
     </View>
+      
   );
 }
