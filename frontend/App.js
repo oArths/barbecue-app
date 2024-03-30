9346a46f96661352eb33a6b33f868dcb4ee618ac
Arthur <arthurferreira1417@gmail.com>
2024-03-29 22:03:44 -0300

    Feat: create a routes bewend pages

--- frontend/App.js
+++ frontend/App.js
@@ -2,15 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import Selecao from "./src/pages/selecao/selecao"
 import Pessoas from "./src/pages/pessoas/pessoas";
 import { View } from "react-native";
-
+// import { NavigationContainer } from '@react-navigation/native';
+// import StackRoutes from "./src/Routes/stack.routes"
 export default function App () {
 
   return (
     <View>
-      <StatusBar style="light" backgroundColor='transparent' translucent/>
+     <StatusBar style="light" backgroundColor='transparent' translucent/>
       <Selecao/>
+      {/* <NavigationContainer>
+      <StackRoutes />
+    </NavigationContainer> */}
 
-    </View>
+    </View> 
       
   );
 }
---
+++ frontend/src/Routes/stack.routes.jsx
@@ -0,0 +1,15 @@
+// import { createNativeStackNavigator } from "@react-navigation/stack";
+
+// import Pessoas from "../pages/pessoas/pessoas";
+// import Selecao from "../pages/selecao/selecao";
+
+// const Stack = createNativeStackNavigator();
+
+// export default function StackRoutes() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="pessoas" component={Pessoas} />
+//       <Stack.Screen name="Selecao" component={Selecao} />
+//     </Stack.Navigator>
+//   );
+// }
