04d7dc4671fd758869b0a4ab8aa8bc31fa4d2e2c
Arthur <arthurferreira1417@gmail.com>
2024-03-29 22:07:37 -0300

    New: styled a imgage inn selecao pages

--- frontend/src/pages/selecao/selecao.jsx
+++ frontend/src/pages/selecao/selecao.jsx
@@ -4,13 +4,22 @@ import ModalOp from "../../components/modal/modal.jsx";
 import React, { useState } from 'react';
 
 
-const  Selecao = () => {
-    const [modalVisible, setModalVisible] = useState(false);
+const  Selecao = ( ) => {
+    // const [modalVisible, setModalVisible] = useState(false);
+    const [modalDeleteVisible, setModalDeleteVisible] = useState(false);
+
+  const abrirModalDelete = () => {
+    setModalDeleteVisible(true);
+  };
+
+  const fecharModalDelete = () => {
+    setModalDeleteVisible(false);
+  };
 
     return(
         <S.Body>
             <S.iconContainer>
-                <S.IconButton>
+                <S.IconButton >
                     <S.Icon source={require("../../img/seta.png")}/>
                 </S.IconButton>
             </S.iconContainer>
@@ -18,40 +27,39 @@ const  Selecao = () => {
                 <S.Title>Selecione os items desejados</S.Title>
             </S.TitleContainer>
             <S.Conatiner>
-                <S.ConatinerOp onPress={() => setModalVisible(true)}>
-                    <S.ConatinerImg
+                <S.ConatinerOp onPress={abrirModalDelete}>
+                    <S.ConatinerBoi
                     source={require('../../img/boi.png')}
                     />
                     <S.ConatinerTitle>Bovina</S.ConatinerTitle>
                 </S.ConatinerOp>
                 <S.ConatinerOp>
-                    <S.ConatinerImg
+                    <S.ConatinerFrango
                     source={require('../../img/chicke.png')}
                     />
                     <S.ConatinerTitle>Frango</S.ConatinerTitle>
                 </S.ConatinerOp>
                 <S.ConatinerOp>
-                    <S.ConatinerImg
+                    <S.ConatinerPorco
                     source={require('../../img/porco.png')}
                     />
                     <S.ConatinerTitle>Porco</S.ConatinerTitle>
                 </S.ConatinerOp>
                 <S.ConatinerOp>
-                    <S.ConatinerImg
+                    <S.ConatinerBebidas
                     source={require('../../img/bebidas.png')}
                     />
                     <S.ConatinerTitle>Bebidas</S.ConatinerTitle>
                 </S.ConatinerOp>
                 <S.ConatinerOp>
-                    <S.ConatinerImg
+                    <S.ConatinerExtras
                     source={require('../../img/extras.png')}
                     />
                     <S.ConatinerTitle>Extra</S.ConatinerTitle>
                 </S.ConatinerOp>
 
-            </S.Conatiner>
-            <ModalOp isVisible={modalVisible} />
-
+            </S.Conatiner >
+            <ModalOp   isVisible={modalDeleteVisible} onClose={fecharModalDelete}/>
             <Button/>
 
 
--- frontend/src/pages/selecao/style.jsx
+++ frontend/src/pages/selecao/style.jsx
@@ -1,78 +1,88 @@
 import styled from "styled-components/native";
 
 export const Body = styled.View`
-display: flex;
-flex-direction: column;
-justify-content: start;
-align-items: center;
-width: 100%;
-height: 100%;
-background-color: #F5F5F5;
-gap: 30px;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #f5f5f5;
+  gap: 30px;
+`;
 export const iconContainer = styled.View`
-display: flex;
-justify-content: center;
-width: 90%;
-margin-top: 70px;
-
+  display: flex;
+  justify-content: center;
+  width: 90%;
+  margin-top: 70px;
 `;
 export const IconButton = styled.TouchableOpacity`
-display: flex;
-justify-content: right;
-align-items: center;
-width: 38px;
-height: 34px;
-
-
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  width: 38px;
+  height: 34px;
 `;
 export const Icon = styled.Image`
-width: 30px;
-height: 26px;
-
-
+  width: 30px;
+  height: 26px;
 `;
 export const TitleContainer = styled.View`
-width: 300px;
-
+  width: 300px;
 `;
 export const Title = styled.Text`
-text-align: center;
-color: black;
-font-size: 30px;
-
+  text-align: center;
+  color: black;
+  font-size: 30px;
 `;
 export const Conatiner = styled.View`
-display: flex;
-flex-direction: row;
-flex-wrap: wrap;
-justify-content: right;
-align-items: center;
-width: 64%;
-gap: 30px;
-background-color: aqua;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: right;
+  align-items: center;
+  width: 64%;
+  gap: 30px;
 `;
 
 export const ConatinerOp = styled.TouchableOpacity`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-background-color: #000000;
-border-radius: 15px;
-padding: 20px;
-width: 110px;
-height: 110px;
-gap: 20px;
-elevation: 2;
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000;
+  border-radius: 15px;
+  padding: 20px;
+  width: 110px;
+  height: 110px;
+  gap: 20px;
+  elevation: 2;
 `;
 export const ConatinerImg = styled.Image`
-width: 55px;
-height: 56px;
-
-`
+  width: 55px;
+  height: 56px;
+`;
+export const ConatinerBoi = styled.Image`
+  width: 60px;
+  height: 40px;
+`;
+export const ConatinerFrango = styled.Image`
+  width: 50px;
+  height: 49px;
+`;
+export const ConatinerPorco = styled.Image`
+  width: 55px;
+  height: 33px;
+`;
+export const ConatinerBebidas = styled.Image`
+  width: 30px;
+  height: 55px;
+`;
+export const ConatinerExtras = styled.Image`
+  width: 60px;
+  height: 50px;
+`;
 export const ConatinerTitle = styled.Text`
-font-size: 18px;
-color: white;
-`
+  font-size: 18px;
+  color: white;
+`;
