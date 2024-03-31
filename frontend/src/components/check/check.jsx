993b3c0f67186dd3a2ece4bfc9b1d7be5e3ecca3
Arthur <arthurferreira1417@gmail.com>
2024-03-31 18:16:02 -0300

    Build: start create the new  checkbox component

---
+++ frontend/src/components/check/check.jsx
@@ -0,0 +1,11 @@
+import * as S from "./style.jsx"
+
+const Check = ({Label}) => {
+    return(
+        <S.Container>
+            <S.OpText>{Label}</S.OpText>
+        </S.Container>
+    )
+}
+
+export default Check;
---
+++ frontend/src/components/check/style.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+
+export const Container = styled.view`
+  display: flex;
+  flex-direction: row;
+`;
+export const ContainerIcon = styled.TouchableOpacity`
+height: 22px;
+border-radius: 4px;
+background-color: aqua;
+justify-content: center;
+align-items: center;
+`;
+export const OpText = styled.text`
+  margin-left: 12px;
+  color: #555;
+  font-size: 16px;
+  font-weight: 600;
+`;
