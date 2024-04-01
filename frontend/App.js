09477e6dfeb1a0f625c91d6c0e1131ffb3c0b39a
Arthur <arthurferreira1417@gmail.com>
2024-04-01 17:17:55 -0300

    New: create routes bewend pages

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import Selecao from "./src/pages/selecao/selecao"
+import Selecao from "./src/pages/selecao/selecao";
 import Pessoas from "./src/pages/pessoas/pessoas";
+import Lista from  "./src/pages/lista/lista"
 import { View } from "react-native";
-// import { NavigationContainer } from '@react-navigation/native';
-// import StackRoutes from "./src/Routes/stack.routes"
-export default function App () {
+import { PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
 
+export default function App() {
   return (
-    <View>
-     <StatusBar style="light" backgroundColor='transparent' translucent/>
-      <Selecao/>
-      {/* <NavigationContainer>
-      <StackRoutes />
-    </NavigationContainer> */}
-
-    </View> 
-      
+    <PaperProvider>
+      <NavigationContainer>
+        <StatusBar style="light" backgroundColor="transparent" translucent />
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="Pessoas"
+            component={Pessoas}
+            options={{ title: null }}
+          />
+          <Stack.Screen
+            name="Selecao"
+            component={Selecao}
+            options={{ title: null }}
+          />
+          <Stack.Screen
+            name="Lista"
+            component={Lista}
+            options={{ title: null }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
